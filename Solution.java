f2d198beb01cd5482bf0803b73ee6a0620234940
chazo <chaz2cool@hotmail.com>
2022-08-20 23:01:49 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,2 +1,41 @@
 public class Solution{
+  
+  public static void main(String[] args){
+    String test = "a1234b";
+    
+    char[] chars = test.toCharArray();
+    int i = 0;
+    int j = test.length-1;
+    
+    while(i>j){
+      boolean isINumber = isNumber(i,chars);
+      boolean isJNumber = isNumber(j,chars);
+      if(!isINumber){
+        i++;
+      }
+      if(!isJNumber){
+        j++;
+      }
+      if(isINumber && isJNumber){
+      
+        char temp = chars[j];
+        chars[j] = chars[i];
+        chars[i] = temp;
+        
+      }
+    
+    }
+    
+    
+    
+  }
+  
+  public boolean isNumber(int location, char[] chars){
+  
+    if(chars[location]>='1' && chars[location]<='9'){
+      return true;
+    }
+  }
+  
+  
 }
