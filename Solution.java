340573774d6e8076ea605d918d742f817933cf54
chazo <chaz2cool@hotmail.com>
2022-08-20 23:14:10 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,40 +1,44 @@
 public class Solution{
   
   public static void main(String[] args){
-    String test = "a1234b";
+    String test = "a01asdfasdfasdf234b";
     
     char[] chars = test.toCharArray();
     int i = 0;
-    int j = test.length-1;
+    int j = test.length()-1;
     
-    while(i>j){
+    while(i<j){
+        System.out.printf("values are %s %s \n" , chars[i], chars[j]);
       boolean isINumber = isNumber(i,chars);
       boolean isJNumber = isNumber(j,chars);
       if(!isINumber){
         i++;
       }
       if(!isJNumber){
-        j++;
+        j--;
       }
       if(isINumber && isJNumber){
       
         char temp = chars[j];
         chars[j] = chars[i];
         chars[i] = temp;
+        i++;
+        j--;
         
       }
     
     }
     
-    
+    System.out.printf("output is %s\n",new String(chars));
     
   }
   
   public static boolean isNumber(int location, char[] chars){
   
-    if(chars[location]>='1' && chars[location]<='9'){
+    if(chars[location]>='0' && chars[location]<='9'){
       return true;
     }
+    return false;
   }
   
   
